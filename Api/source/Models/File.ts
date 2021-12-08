5aca7c3fce104a0f3d6e30e62ac9ae65df3ab47b
unknown <snirajumba@gmail.com>
2021-12-08 11:04:46 -0800

    refactored api , tested api using postman, added model, created data content

---
+++ Api/source/Models/File.ts
@@ -0,0 +1,11 @@
+class File {
+    filename!: String;
+    isDirectory!: boolean;
+    path!: String;
+    size!: String;
+    createdDate!: Date;
+    type!: String;
+    content!: String;
+}
+
+export default File;
--- Api/source/controllers/directory.ts
+++ Api/source/controllers/directory.ts
@@ -1,7 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import axios, { AxiosResponse } from 'axios';
-import IFile from "../Interfaces/IFile"
+import File from "../Models/File";
+
 const fs = require('fs');
+const path = require('path');
+const readFile = require('fs/promises');
 
 // filename, full path, file size, extension/file type and created date
 
@@ -9,30 +12,48 @@ const fs = require('fs');
 const getDirectories = async (req: Request, res: Response, next: NextFunction) => {
     // get some all files and folders
 
-    let result: IFile[] = [] ;
-    const testFolder = __dirname.replace("controllers","data");  
+    let result: File[] = [] ;
+    let content , type;
+    const folderPath = __dirname.replace("controllers","data"); 
+
   
     
-    fs.readdirSync(testFolder).forEach((file: any) => {
-        console.log("file ",file);
-        result.push(file);
+    fs.readdirSync(folderPath).forEach((file: any) => {
+        let  stats= fs.statSync(path.join(folderPath,file));
+        let filename = file; //file name
+        let url = path.join(folderPath,file); //path url
+        let fileSizeInBytes = stats.size; 
+        let size = fileSizeInBytes; // file size in bytes
+        let date  = stats.birthtime; // created date 2021-12-08T12:04:04.915Z
+        
+        let isDirectory = stats.isDirectory();
+        if(isDirectory == false){
+            content = fs.readFileSync(url);
+           type = filename.split('.')[1] // extension
+        }else{
+           content = "null";
+           type = "null";
+        }
+        
+
+       
+        
+        let tempFile : File  = {
+            filename: filename,
+            path: url,
+            size: size,
+            createdDate: date,
+            type: type,
+            content : isDirectory === true ?  "" : content,
+            isDirectory : isDirectory
+        }
+        
+        result.push(tempFile);
     });
-    let Files: IFile[]  = result;
+    let Files: File[]  = result;
     return res.status(200).json({
-        message: testFolder
+        results: result
     });
 };
-
-// // getting a single file or directory
-// const getDirectory = async (req: Request, res: Response, next: NextFunction) => {
-//     // get the file or directory id from the req
-//     let id: string = req.params.id;
-//     // get the file or directory
-//     let result: AxiosResponse = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
-//     let post: Post = result.data;
-//     return res.status(200).json({
-//         message: post
-//     });
-// };
 
 export default { getDirectories};
